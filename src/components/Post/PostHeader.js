2b5a8a8e6830c717403b526cb46adf2ca9e2252b
johnborges <john.borges@icloud.com>
2018-06-27 17:23:46 -0400

    adding action sheet for more button

--- src/components/Post/PostHeader.js
+++ src/components/Post/PostHeader.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
-import { View, Image, Text, StyleSheet } from 'react-native';
+import { View, Image, Text, StyleSheet, TouchableWithoutFeedback, ActionSheetIOS } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default class PostHeader extends Component {
 
+  _onMoreButtonTap() {
+    ActionSheetIOS.showActionSheetWithOptions({
+      options: [
+        'Cancel', 
+        'Share to Facebook', 
+        'Copy Link', 
+        'Turn Off Push Notifications',
+        'Report', // destructive
+        'Mute', // destructive
+        'Unfollow' // destructive
+      ],
+      cancelButtonIndex: 0,
+    },
+    (buttonIndex) => {
+      if (buttonIndex === 1) { /* destructive action */ }
+    });
+  }
+
   render() {
+
     return (
       <View style={styles.container}>
-        <Image 
-          source={{
-            uri: 'https://fakeimg.pl/50x50/'
-          }}
-          style={styles.thumbnail}
-        />
-        <Text>profile_name</Text>
+        <View style={styles.container2}>
+          <Image 
+            source={{
+              uri: 'https://fakeimg.pl/50x50/'
+            }}
+            style={styles.thumbnail}
+          />
+          <Text>profile_name</Text>
+        </View>
+        <TouchableWithoutFeedback onPress={this._onMoreButtonTap}>
+          <Icon name={'ios-more'} size={25} />
+        </TouchableWithoutFeedback>
       </View>
     )
   }
@@ -22,8 +47,14 @@ export default class PostHeader extends Component {
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
     padding: 10,
+    backgroundColor: 'white',
+  },
+  container2: {
+    flexDirection: 'row',
+    alignItems: 'center',
     backgroundColor: 'white',
   },
   thumbnail: {
