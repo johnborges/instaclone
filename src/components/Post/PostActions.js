27c844c4d72bae723be8cb79346189fd61d089eb
johnborges <john.borges@icloud.com>
2018-06-28 20:36:08 -0400

    like action

--- src/components/Post/PostActions.js
+++ src/components/Post/PostActions.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
-import { View, Image, Text, StyleSheet } from 'react-native';
+import { View, TouchableWithoutFeedback, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 export default class PostActions extends Component {
 
+  constructor(props){
+    super(props);
+
+    this.state = {
+      heartFilled: false
+    }
+  }
+
+  _onHeartTap() {
+    this.setState((prevState, props) => ({
+      heartFilled: !prevState.heartFilled
+    }));
+  }
+
   render() {
+
+    let likeBtn = this.state.heartFilled ? 
+      <Icon style={styles.icon} name={'ios-heart'} size={24} color="red"/> : 
+      <Icon style={styles.icon} name={'ios-heart-outline'} size={24}/>;
+
     return (
       <View style={styles.container}>
-        <Icon style={styles.icon} name={'ios-heart-outline'} size={24}/>
+        <TouchableWithoutFeedback onPress={this._onHeartTap.bind(this)}>{likeBtn}</TouchableWithoutFeedback>
         <Icon style={styles.icon} name={'ios-chatbubbles-outline'} size={25}/>
-        <Icon style={styles.icon} name={'ios-redo-outline'} size={25}/>
+        <Icon style={styles.icon} name={'ios-share-alt-outline'} size={25}/>
       </View>
     )
   }
